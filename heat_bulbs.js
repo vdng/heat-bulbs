026ede9a2ced4f462936eec1691796c249f3ee1c
Vincent Duong <duong.vincent@outlook.fr>
2020-02-17 18:09:51 +0100

    added chart

--- heat_bulbs.js
+++ heat_bulbs.js
@@ -1,25 +1,52 @@
-var margin = { top: 40, right: 40, bottom: 50, left: 40 },
-    width = 900 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
-
+// Tooltip
+// =======
 var tooltip = d3.select("body").append("div")
     .attr("class", "hidden tooltip")
-var currentCountryIndice;
+var currentCountryIndice = 0;
 
 // Grille
-var grid = d3.select(".svg-container")
+// ======
+var gridMargin = { top: 40, right: 40, bottom: 50, left: 40 },
+    gridWidth = 900 - gridMargin.left - gridMargin.right,
+    gridHeight = 600 - gridMargin.top - gridMargin.bottom;
+
+var grid = d3.select("#grid")
     .append("svg")
     .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+    .attr("viewBox", "0 0 " + (gridWidth + gridMargin.left + gridMargin.right) + " " + (gridHeight + gridMargin.top + gridMargin.bottom))
     .classed("svg-content-responsive", true)
     .append("g")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+    .attr("transform", "translate(" + gridMargin.left + ", " + gridMargin.top + ")")
     .attr("class", "grid")
 
+// Chart
+// =====
+var chartMargin = { top: 40, right: 40, bottom: 50, left: 40 },
+	chartWidth = 900 - chartMargin.left - chartMargin.right,
+    chartHeight = 600 - chartMargin.top - chartMargin.bottom;
 
-// Echelle de couleur pour les temperatures (juste pour le début, pour visualiser l'évolution des températures sans notion de record)
+var chart = d3.select("#chart")
+	.append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + (chartWidth + chartMargin.left + chartMargin.right) + " " + (chartHeight + chartMargin.top + chartMargin.bottom))
+    .classed("svg-content-responsive", true)
+    .append("g")
+    .attr("transform", "translate(" + gridMargin.left + ", " + gridMargin.top + ")")
+    .attr("class", "chart")
+
+// Echelles
+// ========
+// Grid: Couleur des températures
 var color = d3.scaleSequential().interpolator(d3.interpolateTurbo);
 
+// Chart: Axe Y des températures
+var tempeartureScale = d3.scaleLinear().range([chartHeight, 0]);
+
+// Chart: Axe X du temps
+var yearScale = d3.scaleLinear().range([0, chartWidth]);
+
+// Autres variables
+// ================
 var data = [];
 var rememberRecord = 30;
 var tempToShow = "";
@@ -29,10 +56,8 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
 
     // Parse le temps : 1985-10-23
     var parse = d3.timeParse("%Y-%m-%d");
-    var minYear = 2000,
-        maxYear = 0;
-    var minTemp = 50,
-        maxTemp = -50;
+    var minYear = 2000, maxYear = 0;
+    var minTemp = 50, maxTemp = -50;
 
     data_csv.forEach(d => {
         d.dt = parse(d.dt);
@@ -44,8 +69,12 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
         if (temp > maxTemp) maxTemp = temp;
     })
 
-
     color.domain([minTemp, maxTemp]);
+    tempeartureScale.domain([minTemp, maxTemp]);
+    yearScale.domain([minYear, maxYear]);
+
+    var xAxis = d3.axisBottom().scale(yearScale);
+    var yAxis = d3.axisLeft().scale(tempeartureScale);
 
     console.log('minYear', minYear, 'maxYear', maxYear);
     console.log('minTemp', minTemp, 'maxTemp', maxTemp);
@@ -61,35 +90,32 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
     console.log('dataPerCountry', dataPerCountry);
 
 
-    // TODO : Mettre NULL si pas de donnée 
     // TODO éventuellement : dynamique sur pas que les années mais aussi par saison ou autre période
-    //Nest pour formater les données : regroupées par pays et par année (et on a la moyenne de l'année)
     var tempPerCountryPerYear = d3.nest()
         .key(d => d.Country)
         .key(d => d.dt.getFullYear())
         .rollup(d => { return d3.mean(d, function(e) { return e.AverageTemperature; }) })
-        .object(data_csv)
+        .entries(data_csv)
 
     console.log('tempPerCountryPerYear', tempPerCountryPerYear);
 
     var limitPerCountry = d3.nest()
         .key(d => d.Country)
         .rollup(d => { return { 'minYear': d3.min(d, v => v.dt.getFullYear()), 'maxYear': d3.max(d, v => v.dt.getFullYear()) } })
-        .object(data_csv)
+        .entries(data_csv)
 
 
     console.log('limitPerCountry', limitPerCountry)
 
-    // TODO : année de référence
     for (var i = 0; i < countries.length; i++) {
         data[i] = {
             "country": countries[i],
-            "yearTemperatures": tempPerCountryPerYear[countries[i]],
-            "currentMax": tempPerCountryPerYear[countries[i]][limitPerCountry[countries[i]].minYear],
+            "yearTemperatures": tempPerCountryPerYear[i].values,
+            "currentMax": tempPerCountryPerYear[i].values[0].value,
             "lastBeaten": rememberRecord,
             "currentYearAvailable": false,
-            "minYear": limitPerCountry[countries[i]].minYear,
-            "maxYear": limitPerCountry[countries[i]].maxYear
+            "minYear": limitPerCountry[i].value.minYear,
+            "maxYear": limitPerCountry[i].value.maxYear
         }
     }
 
@@ -98,9 +124,9 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
 
     // Création de la grille
     var n = Math.ceil(Math.sqrt(data.length)); // Nombre de colonnes
-    var square_length = Math.floor(width / n); // longueur d'un côté d'un carré de la grille
-    var rect_width = Math.floor(width / n);
-    var rect_height = Math.floor(height / n);
+    var square_length = Math.floor(gridWidth / n); // longueur d'un côté d'un carré de la grille
+    var rect_width = Math.floor(gridWidth / n);
+    var rect_height = Math.floor(gridHeight / n);
 
     //console.log(maxPerCountry);
 
@@ -119,7 +145,7 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
             d3.select(this)
                 .attr("stroke", "black")
 
-            tempToShow = d.currentYearAvailable ? showTemp(d.yearTemperatures[minYear + yearCount % numYear]) : ""
+            tempToShow = d.currentYearAvailable ? showTemp(d.yearTemperatures[0].value) : ""
 
             tooltip
                 .classed("hidden", false)
@@ -135,6 +161,21 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
         .on('mousemove', function() {
             tooltip.attr("style", "left:" + (d3.event.pageX + 5) + "px; top:" + (d3.event.pageY - 60) + "px");
         })
+        .on('click', function(d, i) {
+        	let line = d3.line()
+        		.x(v => yearScale(v.key))
+        		.y(v => tempeartureScale(v.value))
+        		.curve(d3.curveCardinal);
+
+        	console.log("d.yearTemperatures", d.yearTemperatures)
+
+        	chart.select("path")
+        		.data([d.yearTemperatures])
+        		.transition()
+        		.duration(200)
+        		.attr("d", line)
+
+        })
 
     //Modification de l'affichage de la grille à chaque frame
     var yearCount = 0;
@@ -144,13 +185,38 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
     setInterval(update, windowsDuration) // Durée d'affichage de chaque année
 
     function update() {
+    	// Parcours des pays pour voir si on dépasse le max
+        // Pour le moment, pas ouf : les premières frames affichent beaucoup d'ampoules allumées. C'est normal, il y a beaucoup de chances de casser son record quand on n'a peu de données : il faudrait pas commencer de la première année mais de la n-ème années en initialisant le max au max des 50 premières années
+        //Bug à partir de 60 ans : trouver pourquoi
+        for (var i = 0; i < countries.length; i++) 
+        {
+            if (data[i].minYear - minYear <= yearCount) 
+            {
+                data[i].currentYearAvailable = true;
+	            if (data[i].currentMax < data[i].yearTemperatures[yearCount + minYear - data[i].minYear].value) 
+	            { // Si on dépasse le max
+	                data[i].currentMax = data[i].yearTemperatures[yearCount + minYear - data[i].minYear].value;
+	                data[i].lastBeaten = 0; // Conseil donné par Théo (pas moi, le doctorant) : faire le calcul du booléen maintenant et pas sur le moment de l'affichage
+	            } 
+	            else
+	            {
+	                data[i].lastBeaten += 1;
+	            }
+	        }
+            else
+            {
+                data[i].currentYearAvailable = false;
+	            data[i].lastBeaten += 1;
+            }
+        }
+
         grid.selectAll("rect")
             .transition()
             .duration(windowsDuration)
             .attr("fill", (d, i) => {
                 // Allumage ou pas de la case
                 if (!d.currentYearAvailable) return "#eee"
-                if (d.lastBeaten < rememberRecord) return color(Number(d.yearTemperatures[minYear + yearCount % numYear]))
+                if (d.lastBeaten < rememberRecord) return color(Number(d.yearTemperatures[yearCount + minYear - d.minYear].value))
                 else return d3.color('white')
             })
             .attr("fill-opacity", (d, i) => {
@@ -159,18 +225,16 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
             })
 
         // Affichage texte de l'année
-        d3.select('#year').html('Année : ' + (minYear + yearCount % numYear))
+        d3.select('#year').html('Année : ' + (minYear + yearCount))
 
         var d = data[currentCountryIndice];
-        if (d) {
-            tempToShow = d.currentYearAvailable ? showTemp(d.yearTemperatures[minYear + yearCount % numYear]) : ""
-            tooltip.html(d.country + '<br>' + tempToShow);
-        }
+        tempToShow = d.currentYearAvailable ? showTemp(d.yearTemperatures[yearCount + minYear - d.minYear].value) : ""
+        tooltip.html(d.country + '<br>' + tempToShow);
         /*            if (currentCountryIndice != undefined){
                         var d = data[currentCountryIndice];
                         var tempToShow = '';
                                 if (d.currentYearAvailable){
-                                    tempToShow = Math.floor(10 * d.yearTemperatures[minYear + yearCount % numYear]) / 10 + '°C';
+                                    tempToShow = Math.floor(10 * d.yearTemperatures[minYear + yearCount]) / 10 + '°C';
                                 } 
 
                         tooltip.html(d.country+'<br>'+tempToShow);
@@ -178,29 +242,34 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
 
         yearCount++;
 
-        // Parcours des pays pour voir si on dépasse le max
-        // Pour le moment, pas ouf : les premières frames affichent beaucoup d'ampoules allumées. C'est normal, il y a beaucoup de chances de casser son record quand on n'a peu de données : il faudrait pas commencer de la première année mais de la n-ème années en initialisant le max au max des 50 premières années
-        //Bug à partir de 60 ans : trouver pourquoi
-        for (var i = 0; i < countries.length; i++) {
-
-            if (data[i].yearTemperatures[minYear + yearCount % numYear]) {
-                data[i].currentYearAvailable = true;
-            } else
+        if (yearCount % numYear == 0) 
+        {
+        	console.log("reset")
+        	yearCount = 0;
+            for (var i = 0; i < countries.length; i++) 
+            {
+                data[i].currentMax = tempPerCountryPerYear[i].values[0].value;
+                data[i].lastBeaten = rememberRecord;
                 data[i].currentYearAvailable = false;
-
-            if (data[i].currentMax < data[i].yearTemperatures[minYear + yearCount % numYear]) { //Si on dépasse le max
-                data[i].currentMax = data[i].yearTemperatures[minYear + yearCount % numYear];
-                data[i].lastBeaten = 0; // Conseil donné par Théo (pas moi, le doctorant) : faire le calcul du booléen maintenant et pas sur le moment de l'affichage
-            } else
-                data[i].lastBeaten += 1;
-        }
-
-        if (yearCount % numYear == 0) {
-            for (var i = 0; i < countries.length; i++) {
-                data[i].currentMax = tempPerCountryPerYear[countries[i]][limitPerCountry[countries[i]].minYear];
             }
         }
-    }
+    } // function update()
+
+    // Chart
+    // =====
+    chart.append("path")
+		.attr("fill", "none")
+		.attr("stroke", "#3ac")
+		.attr("stroke-width", 2)
+
+    chart.append("g")
+    	.attr("class", "x-axis")
+    	.attr("transform", "translate(0, " + chartHeight + ")")
+		.call(xAxis)
+
+	chart.append("g")
+	    .attr("class", "y-axis")
+	    .call(yAxis)
 })
 
 function showTemp(temp) {
