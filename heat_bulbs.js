1d4b1a03d15745deb13f34447e4583acd7e17d7d
theosantosecl <theo.santos@ecl16.ec-lyon.fr>
2020-02-19 18:15:32 +0100

    Petit fix du dernier merge

--- heat_bulbs.js
+++ heat_bulbs.js
@@ -365,7 +365,6 @@ d3.csv("https://raw.githubusercontent.com/vdng/heat-bulbs/dev-vincent/GlobalLand
                 .attr("opacity", 0)            
         }
 
-        }
 
         if (buttonOnPlay){	
         	yearCount++;
